b9135ac930ecdd050580cb45ac7875a2a3f33a71
Jangbu Sherpa <sherpa.jangbu7sk@gmail.com>
2023-12-20 16:59:03 -0500

    Added checkmarks

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded",function(){
     let TypeBox = document.getElementById("typeBox")
     let AddBtn = document.getElementById("addBtn")
     let tasksContainer = document.querySelector(".tasks");
+    let messageEl = document.getElementById("message")
     let Tasks = []
     let task = ""
 
@@ -18,30 +19,47 @@ document.addEventListener("DOMContentLoaded",function(){
         }
     })
     function displayTasks(){
+        messageEl.innerHTML=""
         tasksContainer.innerHTML = "";
-        Tasks.forEach(function(task,index){
-            let taskDiv = document.createElement("div");
-            taskDiv.classList.add("taskDiv")
-            
-            let taskText = document.createElement("span")
-            taskText.textContent = task
+        if(Tasks.length === 0){
+            messageEl.innerHTML = "There's no current Tasks"
+        }
+        else{
+            Tasks.forEach(function(task,index){
+                let taskDiv = document.createElement("div");
+                taskDiv.classList.add("taskDiv")
+                
+                let taskText = document.createElement("span")
+                taskText.textContent = task
+                
+                let buttonsDiv = document.createElement("div")
+                buttonsDiv.classList.add("buttonsDiv")
 
-            let deleteButton = document.createElement("button") 
-            let iconElement = document.createElement("i")
-            iconElement.classList.add("fas", "fa-x");
+                let deleteButton = document.createElement("button") 
+                let checkButton = document.createElement("button") 
+                checkButton.classList.add("checkButton")
+                let iconElement1 = document.createElement("i")
+                iconElement1.classList.add("fa-solid", "fa-trash-can");
 
-
-
-
-            deleteButton.addEventListener('click',function(){
-                deleteTask(index)
+                let iconElement2 = document.createElement("i")
+                iconElement2.classList.add("fa-solid","fa-check")
+    
+                deleteButton.addEventListener('click',function(){
+                    deleteTask(index)
+                })
+                
+                
+                tasksContainer.appendChild(taskDiv);
+                taskDiv.appendChild(taskText)
+                deleteButton.appendChild(iconElement1)
+                checkButton.appendChild(iconElement2)
+                buttonsDiv.appendChild(checkButton)
+                buttonsDiv.appendChild(deleteButton)
+                taskDiv.appendChild(buttonsDiv)
+                
             })
-
-            deleteButton.appendChild(iconElement)
-            taskDiv.appendChild(taskText)
-            taskDiv.appendChild(deleteButton)
-            tasksContainer.appendChild(taskDiv);
-        })
+        }
+       
     }
     function deleteTask(index){
         Tasks.splice(index,1)
