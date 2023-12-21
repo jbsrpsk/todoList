4869df442204c4f4ef7b0576d111f1a285ef691c
Jangbu Sherpa <sherpa.jangbu7sk@gmail.com>
2023-12-20 22:21:09 -0500

    Implement task completion and deletion functionality

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ document.addEventListener("DOMContentLoaded",function(){
     let tasksContainer = document.querySelector(".tasks");
     let messageEl = document.getElementById("message")
     let Tasks = []
+    let completedTasks = []
     let task = ""
-    let isGreen = false;
+
 
     AddBtn.addEventListener("click",function(){
         task = TypeBox.value
         if(task != ""){
-            Tasks.push(task)
+            Tasks.push({name:task,completed:false,delete:false})
             TypeBox.value = ""
             displayTasks()
         }
@@ -31,7 +32,7 @@ document.addEventListener("DOMContentLoaded",function(){
                 taskDiv.classList.add("taskDiv")
                 
                 let taskText = document.createElement("span")
-                taskText.textContent = task
+                taskText.textContent = task.name
                 
                 let buttonsDiv = document.createElement("div")
                 buttonsDiv.classList.add("buttonsDiv")
@@ -47,16 +48,25 @@ document.addEventListener("DOMContentLoaded",function(){
                 let iconElement2 = document.createElement("i")
                 iconElement2.classList.add("fa-solid","fa-check")
     
-                deleteButton.addEventListener('click',function(){
-                    deleteTask(index)
-                    
-                })
-                
                 checkButton.addEventListener('click',function(){
-                    isGreen = !isGreen
-                    taskDiv.style.borderBottomColor = isGreen ? 'Green': 'white';
+                    toggleTaskStatus(index)
+                    displayTasks()
                 })
-                
+
+                deleteButton.addEventListener('click',function(){
+                    toggleDeleteStatus(index)
+                    displayTasks()
+                })
+
+
+                if(Tasks[index].completed){
+                    taskDiv.style.borderBottomColor = '#4CB84C'
+                }
+
+                if(Tasks[index].delete){
+                    taskDiv.style.borderBottomColor = 'crimson'
+                }
+
                 tasksContainer.appendChild(taskDiv);
                 taskDiv.appendChild(taskText)
                 deleteButton.appendChild(iconElement1)
@@ -72,7 +82,33 @@ document.addEventListener("DOMContentLoaded",function(){
     function deleteTask(index){
         Tasks.splice(index,1)
         displayTasks()
-        console.log("delete cklicked")
+        console.log("delete clicked")
     }
+    function completeTask(index){
+        completedTasks.push()
+    }
+    function toggleTaskStatus(index){
+        if(Tasks[index].delete){
+            Tasks[index].delete = !Tasks[index].delete
+            console.log("delete is falsed")
+        }
+        else{
+            Tasks[index].completed = !Tasks[index].completed
+            console.log("toggled")
+            console.log(Tasks[index])
+        }
+    }
+    function toggleDeleteStatus(index){
+        if(Tasks[index].completed){
+            Tasks[index].completed= !Tasks[index].completed
+            console.log("completed is falsed")
+        }
+        else{
+            Tasks[index].delete = !Tasks[index].delete
+            console.log("toggled")
+            console.log(Tasks[index])
+        }
+    }
+
 }
 )
